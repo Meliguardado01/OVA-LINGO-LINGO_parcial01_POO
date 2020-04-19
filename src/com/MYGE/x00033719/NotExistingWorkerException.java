274a06eec784c0e3501536db8891c8f3b47aa627
Meliguardado01 <54406833+Meliguardado01@users.noreply.github.com>
2020-04-19 17:00:58 -0600

    Add: NotExistingWorkerException.

---
+++ src/com/MYGE/x00033719/NotExistingWorkerException.java
@@ -0,0 +1,8 @@
+package com.MYGE.x00033719;
+
+public class NotExistingWorkerException extends Exception {
+
+    public NotExistingWorkerException(String message) {
+        super(message);
+    }
+}
