3b13cd971409b9bca4042235b3adaee61df1c9a6
Carlos Enrique Guzman Espinoza <00031119@uca.edu.sv>
2020-04-19 16:56:11 -0600

    Add:NotExistindDocumentException

---
+++ src/com/MYGE/x00033719/NotExistingDocumentException.java
@@ -0,0 +1,8 @@
+package com.MYGE.x00033719;
+
+public class NotExistingDocumentException extends Exception{
+
+    public NotExistingDocumentException(String message) {
+        super(message);
+    }
+}
